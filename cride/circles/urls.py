a093b49008b3722ab0016d889e61b71b37a65d1b
Alexander_Acosta <alexanderacosta@unach.cl>
2021-09-12 16:00:34 -0300

    Cambios en el serializers

--- cride/circles/urls.py
+++ cride/circles/urls.py
@@ -4,8 +4,12 @@
 from django.urls import path
 
 #Views
-from cride.circles.views import list_circles
+from cride.circles.views import list_circles, create_circle
 
 urlpatterns = [
-    path('circles/', list_circles)
+    #path('circles/', list_circles, name='circles'),
+    #path('circles/create/', create_circle, name='create_circle'),
+    
+    path('circles/', list_circles),
+    path('circles/create/', create_circle),
 ]
