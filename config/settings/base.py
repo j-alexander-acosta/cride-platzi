e82f9137eeddb0db1fd53e93555cfe2feab32f42
Alexander_Acosta <alexanderacosta@unach.cl>
2021-09-22 19:01:59 -0300

    avances del proyecto

--- config/settings/base.py
+++ config/settings/base.py
@@ -168,5 +168,5 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 3,
 }
